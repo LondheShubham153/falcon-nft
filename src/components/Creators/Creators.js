6643d311a3681d7dd29b602cbae32fef0220b24d
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-01-29 12:48:15 +0530

    creator section updated

--- src/components/Creators/Creators.js
+++ src/components/Creators/Creators.js
@@ -2,11 +2,24 @@ import React from 'react';
 import classes from "./Creators.module.css"
 
 const Creators = () => {
+  
   return (
   <>
   <h1 className={classes["creator-heading"]} id='creator'>Creators</h1>
   <div className={classes["creators-section"]}>
     <div className={classes.creator}>
+      <img src="https://avatars.githubusercontent.com/u/23608523?v=4" className={classes.image} />
+      <a href='https://github.com/LondheShubham153' className={classes["creator-name"]}>Shubham Londhe</a>
+    </div>
+    <div className={classes.creator}>
+      <img src="https://avatars.githubusercontent.com/u/89961974?v=4" className={classes.image} />
+      <a href='https://github.com/Spyware007' className={classes["creator-name"]}>Om Gawande</a>
+    </div>
+    <div className={classes.creator}>
+      <img src="https://avatars.githubusercontent.com/u/64722289?v=4" className={classes.image} />
+      <a className={classes["creator-name"]}>Toyeesh Sinha</a>
+    </div>
+    {/* <div className={classes.creator}>
       <img src={require("../../assets/logo.jpg")} className={classes.image} />
       <a className={classes["creator-name"]}>Abcdef</a>
     </div>
@@ -21,19 +34,7 @@ const Creators = () => {
     <div className={classes.creator}>
       <img src={require("../../assets/logo.jpg")} className={classes.image} />
       <a className={classes["creator-name"]}>Abcdef</a>
-    </div>
-    <div className={classes.creator}>
-      <img src={require("../../assets/logo.jpg")} className={classes.image} />
-      <a className={classes["creator-name"]}>Abcdef</a>
-    </div>
-    <div className={classes.creator}>
-      <img src={require("../../assets/logo.jpg")} className={classes.image} />
-      <a className={classes["creator-name"]}>Abcdef</a>
-    </div>
-    <div className={classes.creator}>
-      <img src={require("../../assets/logo.jpg")} className={classes.image} />
-      <a className={classes["creator-name"]}>Abcdef</a>
-    </div>
+    </div> */}
   </div>
   </>
     )
