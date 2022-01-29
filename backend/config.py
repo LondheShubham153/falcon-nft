cd7703b5edcbab29375cb3b039517aff808c4397
sinha-toyeesh <toyeshsainik@gmail.com>
2022-01-29 14:25:06 +0530

    Added config.py

---
+++ backend/config.py
@@ -0,0 +1,5 @@
+MONGO_URL_BASE = "https://data.mongodb-api.com/app/data-dazjk/endpoint/data/beta/action/"
+MONGO_URL_GET = MONGO_URL_BASE + "findOne"
+MONGO_URL_POST = MONGO_URL_BASE + "insertOne"
+NFT_IMG = 'https://ichef.bbci.co.uk/news/976/cpsprodpb/DBB7/production/_122074265_hi071843849.jpg'
+API_KEY = 'lQ9bZ2xGOiHUpILGVcTzEFhpUr8d1CJCTCCNiMH3u7aeaEmQ0xLTVPpjCcX5mIjH'
